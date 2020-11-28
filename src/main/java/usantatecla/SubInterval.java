215b8486ee699e579b1079837d147bd4079bd833
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-28 11:27:33 +0100

    Crear clase abstracta SubInterval con las cosas en comun con Min y Max y crear clase abstracta SubIntervalTest con los posibles casos de uso

---
+++ src/main/java/usantatecla/SubInterval.java
@@ -0,0 +1,13 @@
+package usantatecla;
+
+public abstract class SubInterval {
+
+    protected double value;
+
+    public SubInterval(double value) {
+        this.value = value;
+    }
+
+    public abstract boolean isWithin(double value);
+
+}
