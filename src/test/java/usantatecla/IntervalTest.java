a863398579ec37ccd3b266cb602c5842f76cb801
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-28 18:55:08 +0100

    Añadido nuevo test donde el primer intervalo es cerrado-abierto, el segundo es abierto-cerrado y tienen los mismos puntos

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -182,6 +182,15 @@ public class IntervalTest {
         assertTrue(firstInterval.intersects(secondInterval));
     }
 
+    @Test
+    public void givenFirstIntervalClosedOpenWhenSecondIntervalOpenClosedAreTheSameThenShouldIntersect() {
+
+        Interval firstInterval = intervalBuilderInstance().closed(this.left.getEquals()).open(this.right.getEquals()).build();
+        Interval secondInterval = intervalBuilderInstance().open(this.left.getEquals()).closed(this.right.getEquals()).build();
+
+        assertTrue(firstInterval.intersects(secondInterval));
+    }
+
     public static IntervalBuilder intervalBuilderInstance() {
         return new IntervalBuilder();
     }
