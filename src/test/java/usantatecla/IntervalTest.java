25cc073e13104779bf1aa72b07f1805747b911a4
Diego Fernandez Aceves <diego.fernandez@tadaima.studio>
2020-11-28 13:13:40 +0100

    Añadido metodo intersecta con otro intervalo y un test de dos intervalos abiertos donde tienen un punto en comun pero al ser abierto no debe ser intersectado

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -10,18 +10,16 @@ public class IntervalTest {
 
     private Point left = new Point(-2.2);
     private Point right = new Point(4.4);
-    private IntervalBuilder intervalBuilder;
 
     @BeforeEach
     public void before() {
         this.left = new Point(-2.2);
         this.right = new Point(4.4);
-        this.intervalBuilder = new IntervalBuilder();
     }
 
     @Test
     public void givenIntervaOpenOpenlwhenIncludeWithIncludedValueThenTrue() {
-        Interval interval = this.intervalBuilder.open(left.getEquals()).open(right.getEquals()).build();
+        Interval interval = intervalBuilderInstance().open(left.getEquals()).open(right.getEquals()).build();
         assertFalse(interval.include(left.getLess()));
         assertFalse(interval.include(left.getEquals()));
         assertTrue(interval.include(left.getGreater()));
@@ -32,7 +30,7 @@ public class IntervalTest {
 
     @Test
     public void givenIntervaOpenOpenlwhenInc3ludeWithIncludedValueThenTrue() {
-        Interval interval = this.intervalBuilder.closed(left.getEquals()).open(right.getEquals()).build();
+        Interval interval = intervalBuilderInstance().closed(left.getEquals()).open(right.getEquals()).build();
         assertFalse(interval.include(left.getLess()));
         assertTrue(interval.include(left.getEquals()));
         assertTrue(interval.include(left.getGreater()));
@@ -44,7 +42,7 @@ public class IntervalTest {
 
     @Test
     public void givenIntervaOpenOpenlwhenIncludeWit3hIncludedValueThenTrue() {
-        Interval interval = this.intervalBuilder.open(left.getEquals()).closed(right.getEquals()).build();
+        Interval interval = intervalBuilderInstance().open(left.getEquals()).closed(right.getEquals()).build();
         assertFalse(interval.include(left.getLess()));
         assertFalse(interval.include(left.getEquals()));
         assertTrue(interval.include(left.getGreater()));
@@ -56,7 +54,7 @@ public class IntervalTest {
 
     @Test
     public void givenIntervaOpenOpenlwhenIncludeWithInclude5dValueThenTrue() {
-        Interval interval = this.intervalBuilder.closed(left.getEquals()).closed(right.getEquals()).build();
+        Interval interval = intervalBuilderInstance().closed(left.getEquals()).closed(right.getEquals()).build();
         assertFalse(interval.include(left.getLess()));
         assertTrue(interval.include(left.getEquals()));
         assertTrue(interval.include(left.getGreater()));
@@ -64,6 +62,29 @@ public class IntervalTest {
         assertTrue(interval.include(right.getLess()));
         assertTrue(interval.include(right.getEquals()));
         assertFalse(interval.include(right.getGreater()));
+    }
+
+
+    @Test
+    public void givenFirstIntervalOpenOpenWhenSecondIntervalOpenOpenByTheLeftThenShouldNotIntersected() {
+
+        Point left = getPoint(-3.3);
+        Point right = getPoint(-2.2);
+
+        Interval firstInterval = intervalBuilderInstance().open(this.left.getEquals()).open(this.right.getEquals()).build();
+        Interval secondInterval = intervalBuilderInstance().open(left.getEquals()).open(right.getEquals()).build();
+
+        assertFalse(firstInterval.intersects(secondInterval));
+
+
+    }
+
+    public static IntervalBuilder intervalBuilderInstance() {
+        return new IntervalBuilder();
+    }
+
+    public Point getPoint(double value) {
+        return new Point(value);
     }
 
 }
